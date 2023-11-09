3c79521ea2c5ef6d9d767b1e72f2c4a44e29266d
Olcay Taner YILDIZ <olcay.yildiz@ozyegin.edu.tr>
2023-11-09 23:25:32 +0300

    Added string_index.

--- src/StringUtils.c
+++ src/StringUtils.c
@@ -226,7 +226,7 @@ void string_replace(String_ptr st, const char *word) {
     string_append(st, word);
 }
 
-int string_index(const char *s, char **list, int size) {
+int string_index(const char *s, char *list[], int size) {
     for (int i = 0; i < size; i++){
         if (strcmp(s, list[i]) == 0){
             return i;
